50013e4b0fe04acd9eb39aa034d4076c78e2c899
charan96 <charan.sudarsanam@gmail.com>
2016-08-22 20:57:00 -0500

    finished the bokeh oklahoma map for the first page

--- bokehApp/helpers.py
+++ bokehApp/helpers.py
@@ -1,13 +1,33 @@
-codesCSV = "bokehApp/data/codes.csv"
+import pandas as pd
+
+stationMetaData = "bokehApp/data/stationMetadata.csv"
 
 
 def makeCodesDict():
-	with open(codesCSV, 'r') as infile:
-		codesDict = {}
-		for line in infile:
-			code, countyName = line.rstrip("\n").split(',')
-			if countyName in codesDict.keys():
-				codesDict[countyName].append(code)
-			else:
-				codesDict[countyName] = [code, ]
+	"""
+	Read stationMetadata.csv file in data directory and return dict of stations in counties
+	:rtype: dict of counties keyed by County name and values of Station IDs in that county
+	"""
+	codesDict = {}
+	pdReader = pd.read_csv(stationMetaData)
+	pdReader = pdReader[['stid', 'cnty']]
+	for index, row in pdReader.iterrows():
+		if row['cnty'] in codesDict.keys():
+			codesDict[row['cnty']].append(row['stid'])
+		else:
+			codesDict[row['cnty']] = [row['stid'], ]
 	return codesDict
+
+
+def getStations():
+	"""
+	Read stationMetadata.csv file in data directory and return dict of stations
+	:rtype: dict of stations keyed by station ID and values contain a list of the name of station, county name, latitude and longitude
+	"""
+	stations = {}
+	pdReader = pd.read_csv(stationMetaData)
+	pdReader = pdReader[['stid', 'name', 'cnty', 'nlat', 'elon']]
+	for index, row in pdReader.iterrows():
+		stations[row['stid']] = [row['name'], row['cnty'], row['nlat'], row['elon']]
+
+	return stations
--- bokehApp/views.py
+++ bokehApp/views.py
@@ -11,6 +11,7 @@ def index(request):
 	oklahomaCounties = {code: county for code, county in counties.items() if county['state'] == 'ok'}
 
 	codesDict = helpers.makeCodesDict()
+	stations = helpers.getStations()
 
 	countyNames = [county['name'] for county in oklahomaCounties.values()]
 	countyX = [county['lons'] for county in oklahomaCounties.values()]
@@ -21,16 +22,23 @@ def index(request):
 		x=countyX,
 		y=countyY,
 		name=countyNames,
+		stns=stations.keys(),
+		cty=[ct[1] for ct in stations.values()],
 		countyCodes=countyCodes,
 	))
 
 	TOOLS = 'pan,reset,box_zoom,hover,save'
 
-	p = figure(title="Oklahoma Counties", tools=TOOLS, plot_height=500, x_axis_location=None, y_axis_location=None,
-		     plot_width=1000)
+	p = figure(title="Oklahoma Counties", tools=TOOLS, plot_width=1000, plot_height=500, x_axis_location=None,
+		     y_axis_location=None)
 	p.patches('x', 'y', source=source, fill_color='blue', fill_alpha=0.7, line_color='white', line_width=0.5)
 	p.xgrid.grid_line_color = None
 	p.ygrid.grid_line_color = None
+
+	# Marking Stations
+	# for stnID, valList in stations.items():
+	# 	p.circle(valList[3], valList[2], size=3, color="yellow")
+	# p.circle(-97.5085, 35.4819, size=3, color="yellow")
 
 	hover = p.select_one(HoverTool)
 	hover.point_policy = "follow_mouse"
